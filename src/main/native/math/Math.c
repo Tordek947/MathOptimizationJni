77f27743071b19c4f26dbb75a35f2a626d874e0a
Hryhorii Popov <hryhorii_popov@epam.com>
2020-03-05 21:30:56 +0200

    Add c sources to compile for different platform

---
+++ src/main/native/math/Math.c
@@ -0,0 +1,12 @@
+
+
+ extern int fact(int n) {
+	 if (n < 0) {
+		 return -1;
+	 }
+	 int factorial = 1;
+	 for (int i = 2; i <= n; i++) {
+		 factorial *= i;
+	 }
+	 return factorial;
+ }
---
+++ src/main/native/math/Math.i
@@ -0,0 +1,8 @@
+/* math.i */
+ %module Math
+ %{
+ /* Put header files here or function declarations like below */
+ extern int fact(int n);
+%}
+ 
+ extern int fact(int n);
